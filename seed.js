4850979006d95fa1eab1becd40d521f3092011f4
evan lim <evanlim1@evans-MacBook-Pro.local>
2015-05-18 18:03:29 -0400

    fixed seed

--- seed.js
+++ seed.js
@@ -1,22 +1,16 @@
 /*
-
 This seed file is only a placeholder. It should be expanded and altered
 to fit the development of your application.
-
 It uses the same file the server uses to establish
 the database connection:
 --- server/db/index.js
-
 The name of the database used is set in your environment files:
 --- server/env/*
-
 This seed file has a safety check to see if you already have users
 in the database. If you are developing multiple applications with the
 fsg scaffolding, keep in mind that fsg always uses the same database
 name in the environment files.
-
 Refer to the q documentation for why and how q.invoke is used.
-
 */
 
 var NUM_OF_USERS = 10;
